331f9c872dff580774a02170c9f816dc6565084b
Adam <zealan@gmail.com>
2021-02-27 01:24:51 -0500

    commit some non-working request code

--- frontend/script.js
+++ frontend/script.js
@@ -3,5 +3,26 @@ const app = Vue.createApp({
         return {
             five: 5
         };
+    },
+    methods: {
+        sendRequest: function () {
+            console.log('sending request');
+            fetch('https://kicksmarter.ue.r.appspot.com/api', {
+                method: 'POST',
+                body: "{'title': 'adam has a cool title'}",
+                headers:{'content-type': 'application/json'}
+            })
+                .then(function(response) {
+                    return response.json();
+                })
+                .then(function(data) {
+                    console.log(data);
+                    console.log("chance: " + data.chance);
+                    for (var i in data.similar) {
+                        console.log("similar #" + i + " title: " + data.similar[i].title);
+                        console.log("similar #" + i + " url: " + data.similar[i].url);
+                    }
+                });
+        }
     }
 }).mount("#app");
