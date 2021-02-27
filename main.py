d101120c4bbaa365733372c8e5195ae9e4d73d47
Jacob Adams <jacob@tookmund.com>
2021-02-26 23:37:56 -0500

    Initial backend stuff

---
+++ main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+from flask import Flask, jsonify, request
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return jsonify(hello='World!')
+
+@app.route('/api', methods=['POST'])
+def api():
+    # Data in request.form
+    return jsonify({
+        'chance': 50,
+        'similar': [
+                {
+                    'title': 'hello world',
+                    'url': 'https://example.com'
+                }
+            ]
+    })
+
+if __name__ == "__main__":
+    app.run()
