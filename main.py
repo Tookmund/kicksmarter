e21a82b4421ff08d2d1e1fa7615b7ded1e86da78
Jacob Adams <jacob@tookmund.com>
2021-02-27 01:44:20 -0500

    Catch all exceptions

--- main.py
+++ main.py
@@ -12,41 +12,42 @@ def index():
 
 @app.route('/api', methods=['HEAD', 'POST'])
 def api():
-    yourtitle = ''
-    if request.is_json:
-        try:
-            yourtitle = request.get_json()['title']
-        except Exception as e:
-            return jsonify(exception=e)
-    return jsonify({
-        'chance': 50,
-        'similar': [
-                {
-                    'title': yourtitle,
-                    'url': 'https://example.com/your-request',
-                    'category': 'category1',
-                    'subcategory': 'subcat1',
-                    'goal': 1,
-                    'raised': 1,
-                },
-                {
-                    'title': 'title2',
-                    'url': 'https://example.com/url2',
-                    'category': 'category2',
-                    'subcategory': 'subcat2',
-                    'goal': 2,
-                    'raised': 2
-                },
-                {
-                    'title': 'title3',
-                    'url': 'https://example.com/url3',
-                    'category': 'category3',
-                    'subcategory': 'subcat3',
-                    'goal': 3,
-                    'raised': 3
-                },
-            ]
-    })
+    try:
+        yourtitle = ''
+        if request.is_json:
+                yourtitle = request.get_json()['title']
+
+        return jsonify({
+            'chance': 50,
+            'similar': [
+                    {
+                        'title': yourtitle,
+                        'url': 'https://example.com/your-request',
+                        'category': 'category1',
+                        'subcategory': 'subcat1',
+                        'goal': 1,
+                        'raised': 1,
+                    },
+                    {
+                        'title': 'title2',
+                        'url': 'https://example.com/url2',
+                        'category': 'category2',
+                        'subcategory': 'subcat2',
+                        'goal': 2,
+                        'raised': 2
+                    },
+                    {
+                        'title': 'title3',
+                        'url': 'https://example.com/url3',
+                        'category': 'category3',
+                        'subcategory': 'subcat3',
+                        'goal': 3,
+                        'raised': 3
+                    },
+                ]
+        })
+    except Exception as e:
+        return jsonify(exception=e), 400
 
 if __name__ == "__main__":
     app.run()
