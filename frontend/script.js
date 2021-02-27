de869b37682e2f25be1a7a84b3993d39a2154008
Adam <zealan@gmail.com>
2021-02-27 17:40:17 -0500

    add preliminary styles

--- frontend/script.js
+++ frontend/script.js
@@ -30,30 +30,28 @@ const app = Vue.createApp({
             jsonString = JSON.stringify(input);
 
             var vue = this;
-            setTimeout(function() {
-                fetch('https://kicksmarter.ue.r.appspot.com/api', {
-                    method: 'POST',
-                    body: jsonString,
-                    headers: {'Content-Type': 'application/json'}
-                })
-                .then(function(response) {
-                    return response.json();
-                })
-                .then(function(data) {
-                    console.log(data);
-                    vue.chance = data.chance;
-                    while (vue.similar.length > 0) {
-                        vue.similar.pop();
-                    }
-                    for (var i in data.similar) {
-                        vue.similar.push(data.similar[i]);
-                    }
-                    vue.isWaiting = false;
-                })
-                .catch(function(error) {
-                    vue.status = "Error :( Please try again!"
-                });
-            }, 2000);
+            fetch('https://kicksmarter.ue.r.appspot.com/api', {
+                method: 'POST',
+                body: jsonString,
+                headers: {'Content-Type': 'application/json'}
+            })
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(data) {
+                console.log(data);
+                vue.chance = data.chance;
+                while (vue.similar.length > 0) {
+                    vue.similar.pop();
+                }
+                for (var i in data.similar) {
+                    vue.similar.push(data.similar[i]);
+                }
+                vue.isWaiting = false;
+            })
+            .catch(function(error) {
+                vue.status = "Error :( Please try again!"
+            });
         }
     }
 }).mount("#app");
